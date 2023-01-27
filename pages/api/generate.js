ebced5fa6a262a3b1ca747ec7146a5b447a5555d
Augusto Romero <augustoromerodev@gmail.com>
2023-01-27 14:11:00 -0300

    prompt changed for generating Lean Canvaas

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,18 +9,19 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const idea = req.body.idea || "";
+  if (idea.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
-      }
+        message: "Please enter a valid idea",
+      },
     });
     return;
   }
@@ -28,11 +29,15 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
+      prompt: generatePrompt("problem", idea),
       temperature: 0.6,
+      max_tokens: 2622,
+      top_p: 1,
+      frequency_penalty: 0.8,
+      presence_penalty: 0,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,22 +46,34 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(field, idea) {
+  return `Generate the lean canvas for the following idea: ${idea}\n Give me the response in  the following JSON format:${JSON.stringify(
+    LEAN_CANVAS_OBJECT
+  )}`;
 }
+
+const LEAN_CANVAS_OBJECT = {
+  name: "Project Name",
+  problem: "Problem statement",
+  customers: {
+    segments: ["Customer segment 1", "Customer segment 2"],
+    pains: ["Pain point 1", "Pain point 2"],
+    gains: ["Gain 1", "Gain 2"],
+  },
+  solution: {
+    uniqueValueProposition: "Unique value proposition",
+    highLevelConcept: "High level concept",
+    keyMetrics: ["metric 1", "metric 2", "metric n"],
+  },
+  channels: ["Channel 1", "Channel 2"],
+  unfairAdvantages: ["Unfair advantage 1", "Unfair advantage 2"],
+  costStructure: ["cost 1", "cost 2"],
+  revenueStreams: ["Revenue stream 1", "Revenue stream 2"],
+};
